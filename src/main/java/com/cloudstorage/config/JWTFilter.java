891b76396f1c0ac8dbc62e9f41df8d430339c41a
MAYANK KUMAR <109339816+Mayank-mk170@users.noreply.github.com>
2026-08-24 00:24:48 +0530

    Update JWTFilter.java

--- src/main/java/com/cloudstorage/config/JWTFilter.java
+++ src/main/java/com/cloudstorage/config/JWTFilter.java
@@ -28,68 +28,7 @@ public class JWTFilter extends OncePerRequestFilter {
         this.jwtService = jwtService;
         this.userRepository = userRepository;
     }
-//    @Override
-//    protected void doFilterInternal(
-//            HttpServletRequest request,
-//            HttpServletResponse response,
-//            FilterChain filterChain
-//    ) throws ServletException, IOException {
-//
-//        String authorizationHeader =
-//                request.getHeader("Authorization");
-//
-//        if (authorizationHeader != null
-//                && authorizationHeader.startsWith("Bearer ")) {
-//
-//            String token =
-//                    authorizationHeader.substring(7);
-//
-//            try {
-//
-//                // Get email from JWT
-//                String email =
-//                        jwtService.getEmail(token);
-//
-//                // Find user using email
-//                Optional<User> optionalUser =
-//                        userRepository.findByEmail(email);
-//
-//                if (optionalUser.isPresent()
-//                        && SecurityContextHolder
-//                        .getContext()
-//                        .getAuthentication() == null) {
-//
-//                    User user = optionalUser.get();
-//
-//                    // User is the principal
-//                    UsernamePasswordAuthenticationToken authentication =
-//                            new UsernamePasswordAuthenticationToken(
-//                                    user,
-//                                    null,
-//                                    Collections.emptyList()
-//                            );
-//
-//                    authentication.setDetails(
-//                            new WebAuthenticationDetailsSource()
-//                                    .buildDetails(request)
-//                    );
-//
-//                    SecurityContextHolder
-//                            .getContext()
-//                            .setAuthentication(authentication);
-//                }
-//
-//            } catch (Exception e) {
-//
-//                response.setStatus(
-//                        HttpServletResponse.SC_UNAUTHORIZED
-//                );
-//
-//                return;
-//            }
-//        }
-//
-//        filterChain.doFilter(request, response);
+
 @Override
 protected void doFilterInternal(
         HttpServletRequest request,
