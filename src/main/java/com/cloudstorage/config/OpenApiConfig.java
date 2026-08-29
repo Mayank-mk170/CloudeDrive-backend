11a9f991f5e1469246fc0817de3cb69739b1ffaf
Mayank-mk170 <mayankrohan998@gmail.com>
2026-08-29 23:48:36 +0530

    swagger

---
+++ src/main/java/com/cloudstorage/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.cloudstorage.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        Server railwayServer = new Server()
+                .url("https://cloudedrive-backend-production.up.railway.app")
+                .description("Railway Production");
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("CloudDrive API")
+                                .version("1.0")
+                                .description("CloudDrive Backend API")
+                )
+                .servers(
+                        List.of(railwayServer)
+                );
+    }
+}
