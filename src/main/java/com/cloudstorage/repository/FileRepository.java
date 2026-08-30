92df8d892d8782cd20ad94cf290abda0556d45f0
Mayank-mk170 <mayankrohan998@gmail.com>
2026-08-30 23:18:00 +0530

    stars

--- src/main/java/com/cloudstorage/repository/FileRepository.java
+++ src/main/java/com/cloudstorage/repository/FileRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FileRepository extends JpaRepository<File, Long> {
+
     List<File> findByUser(User user);
     Optional<File> findByIdAndUserEmail(
             Long id, String email);
